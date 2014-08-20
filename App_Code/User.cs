f29b02427d41c65c25dbaf70e4c2ef94a388c3da
Chelsea Metcalf <chelseametcalf@ufl.edu>
2014-08-19 23:12:01 -0400

    Created user object.

---
+++ App_Code/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class User
+{
+    public int userID;
+    public string email;
+    public string password;
+    public string firstName;
+    public string lastName;
+    public string address;
+    public string city;
+    public string state;
+    public string zipcode;
+
+	public User()
+	{
+
+	}
+
+    public void create()
+    {
+        string sql = "INSERT INTO Users(email, password, firstName, lastName, address, city, state, zipcode) VALUES('"
+        + email + "', '" + password + "', '" + firstName + "', '" + lastName + "', '" + address + "', '" + city + "', '" + state + "', '" + zipcode + "')";
+    }
+}
