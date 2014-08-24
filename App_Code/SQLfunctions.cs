576b5773d77b599c9db6ecf0699a971cf679c0aa
Chelsea Metcalf <chelseametcalf@ufl.edu>
2014-08-23 23:02:24 -0400

    Added md5 hash and login function. Also connected to database.

---
+++ App_Code/SQLfunctions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+public class SQLfunctions
+{
+    public void executeSQL(string sql)
+    {
+        try
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["PLConnection"].ToString();
+            SqlConnection connect = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connect;
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            connect.Open();
+            cmd.ExecuteNonQuery();
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+
+    public SqlDataReader selectSQL(string sql)
+    {
+        SqlDataReader reader = null;
+        try
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["PLConnection"].ToString();
+            SqlConnection connect = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connect;
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            connect.Open();
+            reader = cmd.ExecuteReader();
+        }
+        catch (Exception e2)
+        {
+
+        }
+        return reader;
+    }
+}
--- App_Code/User.cs
+++ App_Code/User.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 public class User
@@ -15,14 +18,41 @@ public class User
     public string state;
     public string zipcode;
 
-	public User()
-	{
-
-	}
-
     public void create()
     {
         string sql = "INSERT INTO Users(email, password, firstName, lastName, address, city, state, zipcode) VALUES('"
-        + email + "', '" + password + "', '" + firstName + "', '" + lastName + "', '" + address + "', '" + city + "', '" + state + "', '" + zipcode + "')";
+        + email + "', '" + CreateMD5Hash(password) + "', '" + firstName + "', '" + lastName + "', '" + address + "', '" + city + "', '" + state + "', '" + zipcode + "')";
+
+        SQLfunctions sf = new SQLfunctions();
+        sf.executeSQL(sql);
+    }
+
+    public bool login(string pEmail, string pPassword)
+    {
+        SqlDataReader reader = null;
+        string sql = "SELECT 1 FROM Users WHERE email='" + pEmail + "' AND password='" + CreateMD5Hash(pPassword) + "'";
+
+        SQLfunctions sf = new SQLfunctions();
+        reader = sf.selectSQL(sql);
+
+        return reader.HasRows;
+    }
+
+    private string CreateMD5Hash(string input)
+    {
+        // Use input string to calculate MD5 hash
+        MD5 md5 = System.Security.Cryptography.MD5.Create();
+        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+        // Convert the byte array to hexadecimal string
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hashBytes.Length; i++)
+        {
+            sb.Append(hashBytes[i].ToString("X2"));
+            // To force the hex string to lower-case letters instead of
+            // upper-case, use he following line instead:
+            // sb.Append(hashBytes[i].ToString("x2"));
+        }
+        return sb.ToString();
     }
 }
